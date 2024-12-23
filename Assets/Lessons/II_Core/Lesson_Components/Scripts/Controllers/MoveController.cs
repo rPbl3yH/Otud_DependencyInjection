0db41f7b98cb466b4aec7db6a153c84402bc8fb5
Nikolay Terentev <tnv2205@gmail.com>
2024-12-23 20:33:53 +0500

    feat: prepare to lesson

---
+++ Assets/Lessons/II_Core/Lesson_Components/Scripts/Controllers/MoveController.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+
+namespace Lessons.Lesson_Components
+{
+    public class MoveController : MonoBehaviour
+    {
+        [SerializeField] private Character _character;
+
+        private void Update()
+        {
+            HandleKeyboard();
+        }
+
+        private void HandleKeyboard()
+        {
+            Move(Vector3.zero);
+            
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                this.Move(Vector3.forward);
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                this.Move(Vector3.back);
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                this.Move(Vector3.left);
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                this.Move(Vector3.right);
+            }
+        }
+        
+        private void Move(Vector3 direction)
+        {
+            _character.Move(direction);
+        }
+    }
+}
