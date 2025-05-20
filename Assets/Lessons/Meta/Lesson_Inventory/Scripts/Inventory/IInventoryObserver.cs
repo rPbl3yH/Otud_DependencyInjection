7d0a7f9c9aaf8a890c2f8fb91d702e76f263d8f2
Nikolay Terentev <tnv2205@gmail.com>
2025-05-20 21:25:42 +0500

    feat: move to unity 6

--- Assets/Lessons/Meta/Lesson_Inventory/Scripts/Inventory/IInventoryObserver.cs
+++
@@ -1,8 +0,0 @@
-namespace Lessons.Meta.Lesson_Inventory
-{
-    public interface IInventoryObserver
-    {
-        void OnItemAdded(InventoryItem item);
-        void OnItemRemoved(InventoryItem item);
-    }
-}
